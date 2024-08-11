f0d94b728329387c5875a548dd48b7d56c6f7b71
abdelrhman asker <105174274+abdelrhman-asker@users.noreply.github.com>
2024-08-11 19:52:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./Syle.css";
+import "./Syle.scss";
 import App from "./App";
 import DataContextProvider from "./Context/Store";
 
